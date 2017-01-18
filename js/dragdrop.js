064c8bba39e1302e3e5175e9f2186613ad9abf70
Justin Tervay <jtervay97@gmail.com>
2017-01-18 00:55:06 -0500

    UX changes

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -2,6 +2,8 @@
 
 function sort() {
   tinysort('#champs > img', {attr: 'id'});
+  removeEventListeners();
+  addEventListeners();
 }
 
 function allowDrop(ev) {
@@ -14,14 +16,21 @@ function drag(ev) {
 
 function drop(ev) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
+  var movingChamp = ev.dataTransfer.getData("text");
   if (ev.target.tagName == 'IMG') {
-    var node = ev.target.parentNode;
-    document.getElementById('champs').appendChild(node.lastChild);
-    node.appendChild(document.getElementById(data));
+    var targetParent = ev.target.parentNode;
+    var originalParent = document.getElementById(movingChamp).parentNode;
+
+    if (originalParent.id != 'champs') {
+      originalParent.appendChild(document.getElementById(ev.target.id));
+    } else {
+      moveToChamps(targetParent);
+    }
+
+    targetParent.appendChild(document.getElementById(movingChamp));
     sort();
   } else {
-    ev.target.appendChild(document.getElementById(data));
+    ev.target.appendChild(document.getElementById(movingChamp));
   }
 }
 
@@ -33,6 +42,44 @@ function reset() {
       document.getElementById('champs').appendChild(i);
     }
   });
+  sort();
+}
+
+function removeEventListeners() {
+  for (var i = 1; i <= 2; i++) {
+    for (var j = 1; j <= 5; j++) {
+      var str = i + '-' + j;
+      if (j <= 3) {
+        document.getElementById('ban' + str).removeEventListener('dblclick', remove);
+      }
+
+      document.getElementById('pick' + str).addEventListener('dblclick', remove);
+    }
+  }
+}
+
+function addEventListeners() {
+  for (var i = 1; i <= 2; i++) {
+    for (var j = 1; j <= 5; j++) {
+      var str = i + '-' + j;
+      if (j <= 3) {
+        document.getElementById('ban' + str).addEventListener('dblclick', remove);
+      }
+
+      document.getElementById('pick' + str).addEventListener('dblclick', remove);
+    }
+  }
+}
+
+function remove(event) {
+  if (event.srcElement.tagName == 'IMG') {
+    moveToChamps(event.srcElement.parentNode);
+  }
+}
+
+// Make SURE that this is a div element and not an img element
+function moveToChamps(divElement) {
+  document.getElementById('champs').appendChild(divElement.lastChild);
   sort();
 }
 
