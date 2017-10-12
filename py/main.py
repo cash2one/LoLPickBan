4333b6a3ae53f1cbd2517564ba08cba069d69c46
Justin Tervay <jtervay97@gmail.com>
2017-10-12 02:06:02 -0700

    remove f strings

--- py/main.py
+++ py/main.py
@@ -3,28 +3,28 @@ import shutil
 
 
 def get_version():
-	url = f'https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key={api_key}'
+	url = 'https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key={api_key}'.format(api_key=api_key)
 	return requests.get(url).json()[0]
 
 def get_champions(version: str):
-	url = f'http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json'
+	url = 'http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json'.format(version=version)
 	return requests.get(url).json()
 
 def champ_name_to_image_url(version, champ_name):
-	url = f'http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champ_name}.png'
+	url = 'http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champ_name}.png'.format(version=version, champ_name=champ_name)
 	return url
 
 def download_image(url, champ_name):
 	img = requests.get(url, stream=True)
-	with open(f'../img/{champ_name}.png', 'wb+') as fp:
+	with open('../img/{champ_name}.png'.format(champ_name=champ_name), 'wb+') as fp:
 		shutil.copyfileobj(img.raw, fp)
 
 def generate_html(champ_names):
 	icon_size = 64
 	s = ''
 	for chn in champ_names:
-		s += f'<img class="Grid-cell" src="img/{chn}.png" width="{icon_size}" height="{icon_size}" draggable="true"'
-		s += f' ondragstart="drag(event)" id="{chn}">\n'
+		s += '<img class="Grid-cell" src="img/{chn}.png" width="{icon_size}" height="{icon_size}" draggable="true"'.format(chn=chn, icon_size=icon_size)
+		s += ' ondragstart="drag(event)" id="{chn}">\n'.format(chn=chn)
 	return s
 
 if __name__ == '__main__':
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 	version = get_version()
 	champs = get_champions(version)
 	for ch_name in champs['data'].keys():
-		print(f'Downloading {ch_name}...', flush=True)
+		print('Downloading {ch_name}...'.format(ch_name=ch_name), flush=True)
 		download_image(champ_name_to_image_url(version, ch_name), ch_name)
 	html = generate_html(list(champs['data'].keys()))
 	with open('../champs.html', 'w+') as fp:
